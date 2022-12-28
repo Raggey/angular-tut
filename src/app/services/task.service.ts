5d16c66aadb18be0553c38b3d63b77d8394e525c
Craig R <craig.robinson@saberastro.com>
2022-12-29 10:18:59 +1100

    Get request to local backend

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 import { Observable, of } from 'rxjs';
@@ -7,10 +8,11 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
