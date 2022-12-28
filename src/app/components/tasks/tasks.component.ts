1feee64ee5ec86809de646bd1b453f7c6170fc37
Craig R <craig.robinson@saberastro.com>
2022-12-29 10:14:11 +1100

    Obserable service for tasks, starting mock db

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../../Task'
-import { TASKS } from '../../mock-tasks'
-
+import { Task } from '../../Task';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
 
-  tasks: Task[] = TASKS
-
-  constructor() { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks)); // observable - async
   }
-
 }
